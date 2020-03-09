d9b814a16ca1bd3f43b17260839ac0c5a5d7b354
ThomFDV <thomas_ferreira_93330@hotmail.fr>
2020-03-09 16:56:17 +0100

    First commit

---
+++ src/main/java/com/example/J2Eproject/J2EProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.J2Eproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2EProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2EProjectApplication.class, args);
+	}
+
+}
