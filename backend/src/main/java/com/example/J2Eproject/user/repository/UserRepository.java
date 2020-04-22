d74c17f5d4e336929bf1390141e9478196c7a266
Pierre Kettmus <pkettmus@gmail.com>
2020-04-22 18:49:11 +0200

    add mongodb connection with user test

--- backend/src/main/java/com/example/J2Eproject/user/repository/UserRepository.java
+++ backend/src/main/java/com/example/J2Eproject/user/repository/UserRepository.java
@@ -1,10 +1,13 @@
-package com.example.J2Eproject;
+package com.example.J2Eproject.user.repository;
 
+import com.example.J2Eproject.user.model.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends MongoRepository<User, String> {
 
     User findByFirstName(String firstName);
