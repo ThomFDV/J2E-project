a8f07a3287626af76743c62d8ce73151bb8d3483
ThomFDV <thomas_ferreira_93330@hotmail.fr>
2020-06-24 16:21:23 +0200

    Add fields on the user profile to update his account

--- frontend/src/app/components/user/account/account.component.ts
+++ frontend/src/app/components/user/account/account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/models/user';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
@@ -10,17 +11,36 @@ import { User } from 'src/app/models/user';
 export class AccountComponent implements OnInit {
 
   connectedUser: User;
+  opennedForm: Boolean[] = [false, false, false, false];
+  updateForm: FormGroup;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.getProfile();
+    this.updateForm = this.fb.group({
+      email: [],
+      username: [],
+      firstName: [],
+      lastName: []
+    })
   }
 
   getProfile() {
     this.userService.getProfile().subscribe((user: User) => {
       this.connectedUser = user;
     })
+  }
+
+  clearForm(index) {
+    this.opennedForm[index] = !this.opennedForm[index];
+    this.updateForm.reset();
+  }
+
+  updateProfile() {
+    console.log(this.updateForm.value);
+    console.log(this.connectedUser.id);
+    this.updateForm.reset();
   }
 
 }
